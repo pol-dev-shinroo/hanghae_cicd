80b49ef1478c668900357acf215c8e451dee41f6
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-25 22:22:05 +0900

    asdf

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 
-@application.route("/")
+@application.route("/test")
 def hello():
     return "Hello World!"
 
+@application.route("/")
+def main():
+    return render_template("index.html")
 
 # run the app.
 if __name__ == "__main__":
